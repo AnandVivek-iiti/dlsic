14980195bc7d3d8dccfc55ecf492223c39108513
AnandVivek-iiti <anand1234.vivek@gmail.com>
2025-07-01 03:23:36 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,11 @@ import mentorRoutes from './routes/mentorRoutes.js';
 const app = express();
 // const port =" 0.0.0.0";
 const PORT = process.env.PORT || 5000; // Use environment variable or default to 5000
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'https://dlsic-avsr.onrender.com', // Replace with your frontend URL
+  }
+));
 app.use(express.json({ limit: '5mb' }));
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/todo')
@@ -180,7 +184,7 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/api/ping', (req, res) => {
+app.get('https://dlsic-avsr.onrender.com/api/ping', (req, res) => {
   res.json({ message: 'Backend is working fine!' });
 });
 
