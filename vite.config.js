81bfcd3459bbebf8c47d8fb2f1f70b2ca8418f14
AnandVivek-iiti <anand1234.vivek@gmail.com>
2025-07-04 09:26:30 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
+   
   server: {
     port: 5173,
+    historyApiFallback: true,
     proxy: {
       '/api': {
          target: 'https://dlsic-avsr.onrender.com',
-  build: { outDir: 'dist' },
-  base: '/',
+        build: { outDir: 'dist' },
+         base: '/',
         changeOrigin: true,
         secure: false,
       },
