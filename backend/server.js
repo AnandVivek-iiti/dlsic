520adb454aa4174feadd3cdd989c3b182053a706
AnandVivek-iiti <anand1234.vivek@gmail.com>
2025-07-10 00:26:15 +0530

    update Home

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(
 app.use(express.json({ limit: "5mb" }));
 
 mongoose
-  .connect(process.env.MONGO_URL) // || MONGO_URI=mongodb+srv://Anand:Anand@anand.lq8huhq.mongodb.net/
+  .connect(process.env.MONGO_URL || process.env.MONGO_URI) // || MONGO_URI=mongodb+srv://Anand:Anand@anand.lq8huhq.mongodb.net/
 
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
