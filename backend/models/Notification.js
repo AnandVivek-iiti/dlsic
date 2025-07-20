2217772bbf9ac8a115de2c802f206ad49ce88778
Anandvivek-iiti <anand1234.vivek@gmail.com>
2025-07-20 12:29:19 +0530

    updates

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -22,4 +22,5 @@ const notificationSchema = new mongoose.Schema({
   },
 });
 
-export const Notification = mongoose.model("Notification",notificationSchema);
+const Notification = mongoose.models.Notification || mongoose.model("Notification", notificationSchema);
+export default Notification;
